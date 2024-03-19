f3e80bc31c2359b08cf0c72210af089241d7b787
yusra <yusramolina@gmail.com>
2024-03-19 21:24:58 +0100

    B32G4-13_Logout_Yusra

--- src/test/java/com/bleucrm/runners/CukesRunner.java
+++ src/test/java/com/bleucrm/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/bleucrm/step_definitions",
         dryRun = false,
-        tags = "@B32G4-94",
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
