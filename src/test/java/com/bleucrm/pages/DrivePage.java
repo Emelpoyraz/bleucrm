f8d66ba94c873241b2842dc44819dd32b799ff06
Emelpoyraz <emel.poyraz.66@live.com>
2024-03-20 16:34:11 +0200

    added new methods for page access

---
+++ src/test/java/com/bleucrm/pages/DrivePage.java
@@ -0,0 +1,11 @@
+package com.bleucrm.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class DrivePage extends BasePage{
+
+
+}
