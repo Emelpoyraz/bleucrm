d54e48192b0599ec368334914aa15916eeb8c038
Emelpoyraz <emel.poyraz.66@live.com>
2024-03-23 23:18:34 +0200

    login feature is completed

--- src/test/java/com/bleucrm/step_definitions/LoginStepDefs.java
+++ src/test/java/com/bleucrm/step_definitions/LoginStepDefs.java
@@ -2,9 +2,12 @@ package com.bleucrm.step_definitions;
 
 import com.bleucrm.pages.LoginPage;
 import com.bleucrm.utilities.ConfigurationReader;
+import com.bleucrm.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefs {
 
@@ -14,6 +17,8 @@ public class LoginStepDefs {
         System.out.println("Login to app in Before method");
 
     }
+
+    LoginPage loginPage =  new LoginPage();
 
     @Given("the user logged in as {string}")
     public void the_user_logged_in_as(String userType) {
@@ -41,4 +46,66 @@ public class LoginStepDefs {
       loginPage.login(username,password);
     }
 
+
+    @Then("user lands on the {string} successfully")
+    public void userLandsOnTheSuccessfully(String homepageUrl) {
+        String expectedHomePageLink= ConfigurationReader.getProperty("homepageUrl");
+        String actualHomePageLink = Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualHomePageLink.contains(expectedHomePageLink));
+    }
+
+
+    @When("user logged in with username as {string} and password as {string}")
+    public void userLoggedInWithUsernameAsAndPasswordAs(String usernameType, String passwordType) {
+
+        String username="";
+        String password="";
+
+        if(usernameType.equalsIgnoreCase("valid email") && passwordType.equalsIgnoreCase("invalid password")){
+            username = ConfigurationReader.getProperty("hr_username");
+            password = passwordType.replace(" ","");
+        }else if(usernameType.equalsIgnoreCase("invalid email") && passwordType.equalsIgnoreCase("valid password")){
+            username = ConfigurationReader.getProperty("hr_username").replace("hr",usernameType.replace(" ",""));
+            password = ConfigurationReader.getProperty("hr_password");
+        }else if(usernameType.equalsIgnoreCase("invalid email") && passwordType.equalsIgnoreCase("invalid password")){
+            username = ConfigurationReader.getProperty("hr_username").replace("hr",usernameType.replace(" ",""));
+            password = passwordType.replace(" ","");
+
+        }else if(usernameType.equalsIgnoreCase("email") && passwordType.equalsIgnoreCase("no password")){
+            username = ConfigurationReader.getProperty("hr_username");
+        }else if(usernameType.equalsIgnoreCase("no email") && passwordType.equalsIgnoreCase("password")){
+            password = ConfigurationReader.getProperty("help_desk_password");
+        }else if(usernameType.equalsIgnoreCase("no email") && passwordType.equalsIgnoreCase("no password")){
+            username = "";
+            password ="";
+        }
+        //send username and password and login
+        new LoginPage().login(username,password);
+
+    }
+
+    @Then("user should be able to see error message {string}")
+    public void userShouldBeAbleToSeeErrorMessage(String errorMessage) {
+        Assert.assertEquals(errorMessage,loginPage.incorrectLoginOrPasswordMessage.getText());
+    }
+
+    @Then("user should be able to see remember me box")
+    public void userShouldBeAbleToSeeRememberMeBox() {
+        Assert.assertTrue(loginPage.rememberMeCheckBox.isDisplayed());
+    }
+
+    @When("user clicks Remember me checkbox")
+    public void userClicksRememberMeCheckbox() {
+        loginPage.rememberMeCheckBox.click();
+    }
+
+    @Then("user should be able to see Remember me checkbox clicked")
+    public void userShouldBeAbleToSeeRememberMeCheckboxClicked() {
+        Assert.assertTrue(loginPage.rememberMeCheckBox.isSelected());
+    }
+
+    @Then("user should be able to see password is in bullet signs by default")
+    public void userShouldBeAbleToSeePasswordIsInBulletSignsByDefault() {
+        Assert.assertTrue(loginPage.password.getAttribute("type").equals("password"));
+    }
 }
